79c4db70fd742e02e03e1808108c507f5e1ba871
brucemug <mugzbru199@gmail.com>
2023-09-02 14:24:14 +0300

    button for back to home

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails(){
+export default function ReviewDetails({ navigation }){
+
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text>reviewDetails screen</Text>
+            <Button title='back to home' onPress={pressHandler}/>
         </View>
     )
 }
