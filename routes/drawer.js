5593d8b8f9dfb00c85ad4fdce7e2b4cff0019fb4
brucemug <mugzbru199@gmail.com>
2023-09-02 16:16:00 +0300

    added drawer

---
+++ routes/drawer.js
@@ -0,0 +1,7 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createAppContainer } from "react-navigation";
+
+const RootDrawerNavigator = createDrawerNavigator({})
+
+//https://reactnavigation.org/docs/drawer-navigator
