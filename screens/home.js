adf8b76e94a238e7653bd314c702a7efd4912d50
brucemug <mugzbru199@gmail.com>
2023-09-02 15:02:10 +0300

    send data to second screen

--- screens/home.js
+++ screens/home.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home({ navigation }) {
+// export default function Home({ navigation }) {
+//     const pressHandler = () => {
+//         // navigation.navigate('ReviewDetails');
+//         navigation.push('ReviewDetails');
+//     }
 
-    const pressHandler = () => {
-        // navigation.navigate('ReviewDetails');
-        navigation.push('ReviewDetails');
-    }
+//     return (
+//         <View style={globalStyles.container}>
+//             <Text style={globalStyles.titleText}>Home Screen</Text>
+//             <Button title="go to review dates" onPress={pressHandler}/>
+//         </View>
+//     )
+// }
+
+
+export default function Home({ navigation }) {
+    const [reviews, setReviews] = useState([
+        { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+        { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+    ]);
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title="go to review dates" onPress={pressHandler}/>
+            <FlatList 
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
