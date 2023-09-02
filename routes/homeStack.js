a56fad265677110f2a213b78de9818f294775b77
brucemug <mugzbru199@gmail.com>
2023-09-02 16:15:48 +0300

    added headertitles and bg

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,19 +1,38 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-// import * as React from 'react';
+// import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/home";
 import ReviewDetails from "../screens/reviewDetails";
 
-
 const Stack = createStackNavigator();
+
+const defaultNavigationOptions = {
+  headerTintColor: '#444',
+  headerStyle: { backgroundColor: '#87ceeb', height: 70 },
+};
 
 export default function HomeStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ headerTitle: '' }} />
-        <Stack.Screen name="ReviewDetails" component={ReviewDetails} options={{ headerTitle: '' }}/>
+    // <NavigationContainer>
+      <Stack.Navigator screenOptions={defaultNavigationOptions}>
+
+        <Stack.Screen
+          name="Home" 
+          component={Home}
+          options={{
+            headerTitle: "GameZone",
+            // headerStyle: { backgroundColor: "#333" },
+          }}
+        />
+
+        <Stack.Screen
+          name="ReviewDetails"
+          component={ReviewDetails}
+          options={{
+            headerTitle: "Review Details",
+            // headerStyle: { backgroundColor: "#87ceeb" },
+          }}
+        />
       </Stack.Navigator>
-    </NavigationContainer>
+    // </NavigationContainer>
   );
 }
