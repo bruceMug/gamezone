c355679f89e26ca5bcd2f026263fde0823e3bb26
brucemug <mugzbru199@gmail.com>
2023-09-02 16:15:07 +0300

    started aboutStack

---
+++ routes/aboutStack.js
@@ -0,0 +1,28 @@
+import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from "@react-navigation/native";
+import About from "../screens/about";
+
+const Stack = createStackNavigator();
+
+const defaultNavigationOptions = {
+  headerTintColor: '#444',
+  headerStyle: { backgroundColor: '#87ceeb', height: 70 },
+};
+
+export default function AboutStack() {
+  return (
+    // <NavigationContainer>
+      <Stack.Navigator screenOptions={defaultNavigationOptions}>
+
+        <Stack.Screen
+          name="About" 
+          component={About}
+          options={{
+            headerTitle: "About GameZone",
+          }}
+        />
+      </Stack.Navigator>
+    // </NavigationContainer>
+  );
+}
+
